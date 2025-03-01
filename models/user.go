98ab99683c51cd50a84e32dacc49829b7b8af790
Sidharth Sajeev <sidharth2909@gmail.com>
2025-03-02 01:31:04 +0530

    fix(UserModel): Password was not correctly processed for incoming request which makes validation fail

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name     string             `bson:"name" json:"name"`
 	Email    string             `bson:"email" json:"email"`
-	Password string             `bson:"password,omitempty" json:"-"`
+	Password string             `bson:"password,omitempty" json:"password"`
 }
 
 func (User) CollectionName() string {
